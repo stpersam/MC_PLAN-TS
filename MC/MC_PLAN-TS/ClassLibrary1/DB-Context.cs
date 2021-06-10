4c3422fca3c9f933389c38db89981f7be2a4e2da
Franz <wi19b084@technikum-wien.at>
2021-06-10 11:13:08 +0200

    .

---
+++ MC/MC_PLAN-TS/ClassLibrary1/DB-Context.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace [ClassLibrary]_-_Common
+{
+    class DB_Context
+{
+}
+}
