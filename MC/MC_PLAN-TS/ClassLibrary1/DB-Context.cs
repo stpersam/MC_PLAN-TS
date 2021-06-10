57068c4762086944ceee3afaf6437567db226e7e
Franz <wi19b084@technikum-wien.at>
2021-06-10 11:24:56 +0200

    .

--- MC/MC_PLAN-TS/ClassLibrary1/DB-Context.cs
+++ MC/MC_PLAN-TS/ClassLibrary1/DB-Context.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text.Json;
 
 
 namespace _ClassLibrary____Common
 {
+
+    [Table("strings")]
+    public class Manipulationstring
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("ID")]
+        public int EntryId { get; set; }
+        [Column("String_Input")]
+        public string StringInput { get; set; }
+        [Column("Date")]
+        public DateTime Date { get; set; }
+        [Column("Service_Input")]
+        public string ServiceInput { get; set; }
+        [Column("Service_Output")]
+        public string ServiceOutput { get; set; }
+
+    }
+
     class DB_Context
     {
     
